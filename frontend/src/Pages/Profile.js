1c1cb25b3012815def6ee2fadd18fcd1d29fe681
Ruizhe Zhao <ruizhez@usc.edu>
2024-04-22 15:19:26 -0700

    changes to the profile

--- frontend/src/Pages/Profile.js
+++ frontend/src/Pages/Profile.js
@@ -1,9 +1,22 @@
 import TopBar from "../Components/TopBar"
+import './Profile.css';
 
 export default function Profile() {
     return (
         <>
-            <h1>Profile</h1>
+            <TopBar />
+            <div className="logo">LOGO</div>
+            <img className="profile-pic" src="profile-picture.png" alt="Profile Picture" />
+            <div className="info">
+                <p>FIRST NAME: Cinna</p>
+                <p>LAST NAME: Moroll</p>
+                <p>EMAIL: ***oll@gmail.com</p>
+                <p>PASSWORD: ************</p>
+                <button className="edit-btn">EDIT</button>
+                <p>BIRTHDAY: <input type="text" value="03/06" /></p>
+                <p>BIO:</p>
+                <p>I was born on a cloud in the sky, I'm shy but I'm friendly and always willing to make new friends</p>
+            </div>
         </>
     )
 }
